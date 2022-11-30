cebb165675f75116ec26b2842c1c81c573fc1c29
kishore-raghavendra <kishore.raghavendra@mroads.com>
2022-11-30 10:18:42 +0530

    update type

--- src/utils/generateToken.ts
+++ src/utils/generateToken.ts
@@ -1,9 +1,5 @@
 import jwt from 'jsonwebtoken';
-
-type User = {
-  id: number;
-  email: string;
-};
+import { User } from '@prisma/client';
 
 export default function (user: User) {
   return jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET!);
